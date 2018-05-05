b927cb88ffe6ffc817ccf314ebe2910a54d32d62
Iago George <iagogeorge20@gmail.com>
2018-05-05 18:35:20 -0300

    remocao de arquivos desnecessarios

--- WebContent/js/mask.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($){
-    $("#cpf").mask("999.999.999-99");
-    $("#dataNasc").mask("99/99/9999");
-    $("#telefone").mask("(99)999999999");
-    $("#cep").mask("99999999");
-    $("#numero").mask("9999999");
-    $("#uf").mask("AA");
-});
